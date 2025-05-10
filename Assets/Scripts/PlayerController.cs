beb30c4cb6c7f5ec945e7ee9c9bd55a156ec1176
aleksandraseverova <aleksandraseverova09@gmail.com>
2025-05-11 00:51:35 +0300

    Ruslana
    +

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,27 +2,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float movementSpeed = 3f;
     float currentSpeed;
+    [SerializeField] float shiftSpeed = 10f;
+    [SerializeField] float jumpForce = 7f;
+    bool isGrounded = true;
     Animator anim;
     Rigidbody rb;
     Vector3 direction;
-    float jumpForce = 7f;
-    bool isGrounded;
+    private int health;
+    float stamina = 5f;
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        currentSpeed = movementSpeed;     
-        anim = GetComponent<Animator>();   
+        rb = GetComponent<Rigidbody>();   
+        anim = GetComponent<Animator>(); 
+		currentSpeed = movementSpeed;  		
+        health = 100;	
     }
 
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
+              
         direction = new Vector3(moveHorizontal, 0.0f, moveVertical);
         direction = transform.TransformDirection(direction);
+        anim.SetFloat("Speed",Vector3.ClampMagnitude(direction * currentSpeed, 1).magnitude);
+    
+        
         
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
         {
@@ -33,13 +40,40 @@ public class PlayerController : MonoBehaviour
 
         if(direction.x !=0 || direction.z !=0)
         {
-            anim.SetBool("Walk", true);
+            // anim.SetBool("Walk", true);
         }
         if (direction.x == 0 && direction.z == 0)
         {
-            anim.SetBool("Walk", false);
+            // anim.SetBool("Walk", false);
+        }   
+	    if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = shiftSpeed;
+            anim.SetBool("running", true);
         }
-    }
+        if (!Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = movementSpeed;
+            anim.SetBool("running", false);
+        }
+        // if (Input.GetKey(KeyCode.LeftShift))
+        // {
+        //     if (stamina > 0)
+        //     {
+        //         stamina -= Time.deltaTime;
+        //         currentSpeed = shiftSpeed;
+        //     }
+        //     else
+        //     {
+        //         currentSpeed = movementSpeed;
+        //     }
+        // }
+        // else if (!Input.GetKey(KeyCode.LeftShift))
+        // {
+        //     stamina += Time.deltaTime;
+        //     currentSpeed = movementSpeed;
+        // }
+	}
 
     void FixedUpdate()
     {
